e666ee9bbb7af30cb3fffbd14d9144d1cc660c77
Robert Kowalski <robin1315@onet.pl>
2016-04-04 23:29:21 +0200

    change2

--- app.py
+++ app.py
@@ -3,23 +3,15 @@ import os
 from flask import Flask
 from flask import render_template
 from flask.ext.sqlalchemy import SQLAlchemy
+from models import *
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80))
-    email = db.Column(db.String(120), unique=True)
-
-    def __init__(self, name, email):
-        self.name = name
-        self.email = email
-
-    def __repr__(self):
-        return '<Name %r>' % self.name
+user = User('John Doe', 'john.doe@example.com')
+db.session.add(user)
+db.session.commit()
 
 
 @app.route('/')
