8890c22a94a89c6781b35c5a48c81cfed69a06b7
Robert Kowalski <robin1315@onet.pl>
2016-04-12 14:36:24 +0200

    asd

--- models.py
+++ models.py
@@ -5,53 +5,6 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    loginu = db.Column(db.String)
-    name = db.Column(db.String(80))
-    surname = db.Column(db.String(80))
-    email = db.Column(db.String(120), unique=True)
-
-    def __init__(self, name, surname, loginu, email):
-        self.name = name
-        self.email = email
-        self.surname = surname
-        self.loginu = loginu
-
-    def __repr__(self):
-        return '<Name %r %r> Email %r' % self.name, self.surname, self.email
-
-
-class Cars(db.Model):
-    idcar = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    nrrej = db.Column(db.String(9))
-    iduser = db.Column(db.Integer)  # dopisac foreign key User id
-    mark = db.Column(db.String(20))
-
-
-class Park(db.Model):
-    idpark = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(40))
-    street = db.Column(db.String(30))
-    street_nr = db.Column(db.Integer)
-    city = db.Column(db.String(20))
-
-    def __init__(self, name, str, str_nr, ci):
-        self.name = name
-        self.street = str
-        self.street_nr = str_nr
-        self.city = ci
-
-    def as_dict(self):
-        obc_dict = {
-            'ID': self.idpark,
-            'Nazwa': self.name,
-            'Ulica': self.street,
-            'Nr_ulicy': self.street_nr,
-            'Miasto': self.city
-        }
-        return obc_dict
-
 
 # create table Stanowiska
 # (IDStanowiska int primary key identity(1,1)
